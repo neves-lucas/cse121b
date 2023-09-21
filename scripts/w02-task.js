86da9e319e9111667191e55fc3510625015588df
Lucas <neves.lucas.r@gmail.com>
2023-09-21 10:31:19 -0300

    fix attempt

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -6,23 +6,32 @@
 const fullName = 'Lucas Neves Rocha';
 const currentYear = new Date().getFullYear();
 const profilePicture = 'images/selfie.jpg'
-const title = 'W02-Task - Profile Home Page';
+const title = 'Lucas Neves Rocha | CSE 121B | BYU-Idaho';
 
 /* Step 3 - Element Variables */
 const nameElement = document.getElementById('name');
 const foodElement = document.getElementById('food');
 const yearElement = document.querySelector('#year');
 const imageElement = document.querySelector('img');
+const titleElement = document.querySelector('title');
+const foodTitle = document.querySelector('h2');
+const hr = document.createElement('hr');
 
 /* Step 4 - Adding Content */
 nameElement.innerHTML = `<strong>${fullName}</strong>`;
 yearElement.textContent = `Year: ${currentYear}`;
 imageElement.setAttribute('src', profilePicture);
 imageElement.setAttribute('alt', `Profile image of ${fullName}`);
+titleElement.innerHTML = title;
+foodTitle.after(hr);
 
 /* Step 5 - Array */
-let favoriteFoods = ['Pizza', 'Hamburger', 'Sushi', 'Lasagna', 'Pasta'];
-foodElement.innerHTML = favoriteFoods.join(',');
+let favoriteFoods = [];
+favoriteFoods.push('Pizza');
+favoriteFoods.push('Hamburguer');
+favoriteFoods.push('Sushi');
+favoriteFoods.push('Lasagna');
+favoriteFoods.push('Pasta');
 const newFood = ['Ice Cream'];
 favoriteFoods.push(newFood);
 foodElement.innerHTML += `<br>${favoriteFoods}`

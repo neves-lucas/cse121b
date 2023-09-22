d39f1a8e3210e7387dcfe7b3496b7d5eaa6cdb43
Lucas <neves.lucas.r@gmail.com>
2023-09-22 18:27:31 -0300

    fixes

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -4,19 +4,19 @@
 
 /* Step 2 - Variables */
 const fullName = 'Lucas Neves Rocha';
-const currentYear = new Date().getFullYear();
-const profilePicture = 'images/selfie.jpg'
+let currentYear = new Date().getFullYear();
+let profilePicture = 'images/selfie.jpg'
 
 /* Step 3 - Element Variables */
 const nameElement = document.getElementById('name');
 const foodElement = document.getElementById('food');
-const yearElement = document.querySelector('#year');
-const imageElement = document.querySelector('img');
+let yearElement = document.querySelector('#year');
+let imageElement = document.querySelector('img');
 
 
 /* Step 4 - Adding Content */
 nameElement.innerHTML = `<strong>${fullName}</strong>`;
-yearElement.textContent = `Year: ${currentYear}`;
+yearElement.textContent = currentYear;
 imageElement.setAttribute('src', profilePicture);
 imageElement.setAttribute('alt', `Profile image of ${fullName}`);
 

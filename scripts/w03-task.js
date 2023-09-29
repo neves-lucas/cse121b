e6e0ac65749bffe055e891f7a3899eb4a3be57fb
Lucas <neves.lucas.r@gmail.com>
2023-09-29 16:36:26 -0300

    path fixing

---
+++ scripts/w03-task.js
@@ -0,0 +1,36 @@
+/* LESSON 3 - Programming Tasks */
+
+/* FUNCTIONS */
+/* Function Definition - Add Numbers */
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+let addNumbers = Number.add(document.querySelector('#add1').value);
+document.querySelector('#sum').value = add(addNumber1, addNumber2);
+document.querySelector('addNumbers').addEventListener('click', addNumbers);
+
+/* Function Expression - Subtract Numbers */
+
+
+/* Arrow Function - Multiply Numbers */
+
+
+/* Open Function Use - Divide Numbers */
+
+
+/* Decision Structure */
+
+
+/* ARRAY METHODS - Functional Programming */
+/* Output Source Array */
+
+/* Output Odds Only Array */
+
+/* Output Evens Only Array */
+
+/* Output Sum of Org. Array */
+
+/* Output Multiplied by 2 Array */
+
+/* Output Sum of Multiplied by 2 Array */
